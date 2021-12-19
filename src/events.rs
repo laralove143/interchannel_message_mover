b52e0ed53c891bc790c4f1c04cee5ced4d8187aa
laralove143 <laralove143@icloud.com>
2021-12-19 14:35:58 +0300

    insert messages on messagecreate

--- src/events.rs
+++ src/events.rs
@@ -1,13 +1,19 @@
-use anyhow::anyhow;
+use anyhow::{bail, Result};
 use twilight_gateway::Event;
 
 use crate::{commands, Context};
 
 pub async fn handle(ctx: Context, event: Event) {
-    if let Err(err) = match event {
-        Event::InteractionCreate(interaction) => commands::handle(ctx, interaction.0).await,
-        _ => Err(anyhow!("unexpected event: {:?}", event)),
-    } {
+    if let Err(err) = _handle(ctx, event).await {
         eprintln!("{}", err);
     }
+}
+
+async fn _handle(ctx: Context, event: Event) -> Result<()> {
+    match event {
+        Event::InteractionCreate(interaction) => commands::handle(ctx, interaction.0).await?,
+        Event::MessageCreate(message) => ctx.cache.add_message(message.0),
+        _ => bail!("unexpected event: {:?}", event),
+    }
+    Ok(())
 }
