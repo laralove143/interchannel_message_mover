618610fa3c469db68ba2fbeed95fa336673920b9
laralove143 <laralove143@icloud.com>
2021-12-20 22:30:24 +0300

    update messages cache on message edits

--- src/events.rs
+++ src/events.rs
@@ -13,6 +13,7 @@ async fn _handle(ctx: Context, event: Event) -> Result<()> {
     match event {
         Event::InteractionCreate(interaction) => commands::handle(ctx, interaction.0).await?,
         Event::MessageCreate(message) => ctx.cache.add_message(message.0),
+        Event::MessageUpdate(message) => ctx.cache.update_message(*message),
         _ => bail!("unexpected event: {:?}", event),
     }
     Ok(())
