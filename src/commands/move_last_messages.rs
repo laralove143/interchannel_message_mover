32d4adbb0d485c9ad6215300a2a96aba22623426
laralove143 <laralove143@icloud.com>
2021-12-24 16:29:05 +0000

    create, cache, retreive and execute webhooks

--- src/commands/move_last_messages.rs
+++ src/commands/move_last_messages.rs
@@ -5,7 +5,7 @@ use twilight_model::{
     guild::Permissions,
 };
 
-use crate::{cache::MessageContent, Context};
+use crate::Context;
 
 #[derive(CreateCommand, CommandModel)]
 #[command(
@@ -29,39 +29,73 @@ pub struct MoveLastMessages {
 pub async fn run(ctx: Context, command: ApplicationCommand) -> Result<impl Into<String>> {
     let channel_id = command.channel_id;
 
+    let options = MoveLastMessages::from_interaction(command.data.into())?;
+
+    // TODO: let only admins use this command
+    if !options
+        .channel
+        .permissions
+        .contains(Permissions::SEND_MESSAGES | Permissions::MANAGE_WEBHOOKS)
+    {
+        return Ok("i need the **send messages** and **manage webhooks** permissions for that");
+    }
+
     if !ctx
         .twilight_cache
         .permissions()
         .in_channel(ctx.cache.user_id, channel_id)?
         .contains(Permissions::MANAGE_MESSAGES)
     {
-        return Ok("give me the manage messages permission first please");
+        return Ok("i need the **manage messages** permission for that");
     }
 
-    let options = MoveLastMessages::from_interaction(command.data.into())?;
     let messages = match ctx.cache.get_messages(channel_id) {
         Some(messages) => messages,
         None => {
-            return Ok("looks like i couldn't read anything here :( check my permissions please")
+            return Ok(
+                "looks like i couldn't read anything here :( make sure i have **view channels** \
+                 permission",
+            )
         }
     };
 
-    let (content, embeds, message_ids) = messages
+    let target_channel_id = options.channel.id;
+
+    let webhook = match ctx.cache.get_webhook(target_channel_id) {
+        Some(webhook) => webhook,
+        None => {
+            let webhook = ctx
+                .http
+                .create_webhook(target_channel_id, "message transit")
+                .exec()
+                .await?
+                .model()
+                .await?;
+            ctx.cache.add_webhook(webhook);
+            ctx.cache.get_webhook(target_channel_id).unwrap()
+        }
+    };
+
+    let (content, message_ids) = messages
         .iter()
         .rev()
         .take(options.message_count as usize)
         .rfold(
-            (String::new(), Vec::new(), Vec::new()),
-            |(mut message_content, mut message_embeds, mut message_ids), message| {
-                if let MessageContent::Valid { content, embeds } = &message.content {
-                    message_content.push_str(&content);
-                    message_embeds.extend(embeds);
-                    message_content.push('\n');
-                    message_ids.push(message.id);
-                }
-                (message_content, message_embeds, message_ids)
+            (String::new(), Vec::new()),
+            |(mut content, mut message_ids), message| {
+                content.push_str(&message.content);
+                content.push('\n');
+                message_ids.push(message.id);
+
+                (content, message_ids)
             },
         );
+
+    ctx.http
+        .execute_webhook(webhook.id, &webhook.token)
+        .content(&content)
+        .exec()
+        .await?;
 
     ctx.http
         .delete_messages(channel_id, &message_ids)
