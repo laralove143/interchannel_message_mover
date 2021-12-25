aafc12fc34bbc0f826391c944cd206c4a4a0a56e
laralove143 <laralove143@icloud.com>
2021-12-25 21:55:39 +0000

    update cache on webhooks update

--- src/events.rs
+++ src/events.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use twilight_gateway::Event;
 
-use crate::{commands, Context};
+use crate::{commands, Cache, Context};
 
 pub async fn handle(ctx: Context, event: Event) {
     if let Err(err) = _handle(ctx, event).await {
@@ -16,6 +16,7 @@ async fn _handle(ctx: Context, event: Event) -> Result<()> {
         Event::MessageUpdate(message) => ctx.cache.update_message(*message),
         Event::MessageDelete(message) => ctx.cache.delete_message(message),
         Event::MessageDeleteBulk(messages) => ctx.cache.delete_messages(messages),
+        Event::WebhooksUpdate(update) => Cache::update_webhooks(ctx, update.channel_id).await?,
         _ => (),
     }
     Ok(())
