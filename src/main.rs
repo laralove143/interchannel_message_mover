93b877bed80a3994f089d6a7a137ad3b0a34030d
laralove143 <laralove143@icloud.com>
2022-02-06 23:37:09 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-//! description = "a simple discord bot to move messages between channels"
-#![allow(clippy::shadow_same, clippy::implicit_return)]
+//! "a simple discord bot to move messages between channels"
+// TODO! check docs
+#![allow(
+    clippy::shadow_same,
+    clippy::implicit_return,
+    clippy::unseparated_literal_suffix,
+    clippy::pattern_type_mismatch
+)]
 
 /// interaction commands
 mod commands;
@@ -20,6 +26,7 @@ use twilight_model::id::{
     marker::{ApplicationMarker, ChannelMarker, UserMarker},
     Id,
 };
+use twilight_standby::Standby;
 use webhooks::CachedWebhook;
 
 /// thread safe context
@@ -29,6 +36,8 @@ type Context = Arc<ContextValue>;
 pub struct ContextValue {
     /// used to make http requests
     http: Client,
+    /// used to wait for everyone to agree on the move
+    standby: Standby,
     /// used to cache permissions and messages
     cache: InMemoryCache,
     /// webhooks cache
@@ -39,9 +48,35 @@ pub struct ContextValue {
     application_id: Id<ApplicationMarker>,
 }
 
+impl ContextValue {
+    /// creates a new context value:
+    async fn new(resource_types: ResourceType, http: Client) -> Result<Self> {
+        Ok(Self {
+            standby: Standby::new(),
+            cache: InMemoryCache::builder()
+                .resource_types(resource_types)
+                .message_cache_size(20)
+                .build(),
+            webhooks: DashMap::new(),
+            user_id: http.current_user().exec().await?.model().await?.id,
+            application_id: http
+                .current_user_application()
+                .exec()
+                .await?
+                .model()
+                .await?
+                .id,
+            http,
+        })
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let intents = Intents::GUILD_MESSAGES | Intents::GUILDS | Intents::GUILD_WEBHOOKS;
+    let intents = Intents::GUILD_MESSAGES
+        | Intents::GUILDS
+        | Intents::GUILD_MEMBERS
+        | Intents::GUILD_WEBHOOKS;
     let event_types = EventTypeFlags::INTERACTION_CREATE
         | EventTypeFlags::MESSAGE_CREATE
         | EventTypeFlags::MESSAGE_UPDATE
@@ -59,7 +94,8 @@ async fn main() -> Result<()> {
         | EventTypeFlags::CHANNEL_DELETE
         | EventTypeFlags::MEMBER_ADD
         | EventTypeFlags::MEMBER_UPDATE
-        | EventTypeFlags::MEMBER_REMOVE;
+        | EventTypeFlags::MEMBER_REMOVE
+        | EventTypeFlags::MEMBER_CHUNK;
     let resource_types = ResourceType::MESSAGE
         | ResourceType::GUILD
         | ResourceType::ROLE
@@ -72,32 +108,18 @@ async fn main() -> Result<()> {
         .event_types(event_types)
         .build()
         .await?;
-    let cluster_spawn = Arc::new(cluster);
-    tokio::spawn(async move { cluster_spawn.up().await });
+    cluster.up().await;
 
     let http = Client::new(token);
 
-    let ctx = Arc::new(ContextValue {
-        cache: InMemoryCache::builder()
-            .resource_types(resource_types)
-            .message_cache_size(20)
-            .build(),
-        webhooks: DashMap::new(),
-        user_id: http.current_user().exec().await?.model().await?.id,
-        application_id: http
-            .current_user_application()
-            .exec()
-            .await?
-            .model()
-            .await?
-            .id,
-        http,
-    });
+    let ctx = Arc::new(ContextValue::new(resource_types, http).await?);
 
     commands::create(&ctx).await?;
 
-    while let Some((_, event)) = events.next().await {
+    while let Some((shard_id, event)) = events.next().await {
+        ctx.standby.process(&event);
         ctx.cache.update(&event);
+        events::request_members(&cluster, shard_id, &event).await?;
         tokio::spawn(events::handle(Arc::clone(&ctx), event));
     }
 
