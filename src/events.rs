c0db9b6fde69294d7002c08a1f61f5127700e6dd
laralove143 <laralove143@icloud.com>
2022-02-06 23:35:30 +0300

    request members from the cluster to cache

--- src/events.rs
+++ src/events.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
-use twilight_gateway::Event;
+use twilight_gateway::{Cluster, Event};
+use twilight_model::gateway::payload::outgoing::RequestGuildMembers;
 
 use crate::{commands, webhooks, Context};
 
-/// handles incoming events, prints on error
+/// handles the event, prints the returned error to stderr
 #[allow(clippy::print_stderr)]
 pub async fn handle(ctx: Context, event: Event) {
     if let Err(err) = _handle(ctx, event).await {
@@ -11,13 +12,29 @@ pub async fn handle(ctx: Context, event: Event) {
     }
 }
 
+/// handles the event, passing on the returned error
 #[allow(clippy::wildcard_enum_match_arm)]
-/// handles the event, returns the inner error
 async fn _handle(ctx: Context, event: Event) -> Result<()> {
     match event {
         Event::InteractionCreate(interaction) => commands::handle(ctx, interaction.0).await?,
         Event::WebhooksUpdate(update) => webhooks::update(ctx, update.channel_id).await?,
         _ => (),
     }
+    Ok(())
+}
+
+/// if the event is a guild create event, sends the shard a command to request
+/// the members and prints a message
+#[allow(clippy::print_stdout)]
+pub async fn request_members(cluster: &Cluster, shard_id: u64, event: &Event) -> Result<()> {
+    if let Event::GuildCreate(guild) = event {
+        cluster
+            .command(
+                shard_id,
+                &RequestGuildMembers::builder(guild.id).query("", None),
+            )
+            .await?;
+        println!("requested members");
+    };
     Ok(())
 }
