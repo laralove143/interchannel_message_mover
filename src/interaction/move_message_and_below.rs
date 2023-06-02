508b5dfc1ba1effbb9c7a3758333ee49f2aed109
laralove <laralove143@icloud.com>
2023-06-02 22:54:16 +0300

    remove limits for scaling

--- src/interaction/move_message_and_below.rs
+++ src/interaction/move_message_and_below.rs
@@ -1,11 +1,11 @@
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use anyhow::Result;
-use sparkle_convenience::reply::Reply;
+use sparkle_convenience::{error::IntoError, reply::Reply};
 use twilight_model::application::command::{Command, CommandType};
 use twilight_util::builder::command::CommandBuilder;
 
-use crate::{interaction::InteractionContext, CustomError};
+use crate::interaction::InteractionContext;
 
 pub const NAME: &str = "move this message and below";
 
@@ -17,13 +17,9 @@ pub fn command() -> Command {
 
 impl InteractionContext<'_> {
     pub async fn handle_move_message_and_below_command(self) -> Result<()> {
+        let guild_id = self.interaction.guild_id.ok()?;
+
         let mut messages = vec![self.handle_message_command()?];
-        if (SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs()
-            - u64::try_from(messages[0].timestamp.as_secs())?)
-            > 2 * 7 * 24 * 60 * 60
-        {
-            return Err(CustomError::MessageTooOld.into());
-        }
 
         let channel = self.wait_for_channel_select_interaction().await?;
 
@@ -44,8 +40,7 @@ impl InteractionContext<'_> {
             11..=20 => "starting up the truck :pickup_truck:",
             21..=30 => "starting up the truck :truck:",
             31..=40 => "starting up the lorry :articulated_lorry:",
-            41..=50 => "starting up the ship :ship:",
-            _ => return Err(CustomError::TooManyMessages.into()),
+            _ => "starting up the ship :ship:",
         };
         self.handle
             .reply(
@@ -56,19 +51,43 @@ impl InteractionContext<'_> {
             )
             .await?;
 
-        for message in &messages {
+        for (idx, message) in messages.iter().enumerate() {
+            if (idx + 1) % 10 == 0 {
+                println!(
+                    "moving messages in {guild_id}: {}/{}",
+                    idx + 1,
+                    messages.len()
+                );
+            }
+
             self.ctx
                 .execute_webhook_as_member(message, &channel)
                 .await?;
             tokio::time::sleep(Duration::from_secs(1)).await;
         }
 
-        if messages.len() == 1 {
-            self.ctx
-                .bot
-                .http
-                .delete_message(messages[0].channel_id, messages[0].id)
-                .await?;
+        if (SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs()
+            - u64::try_from(messages[0].timestamp.as_secs())?)
+            > 2 * 7 * 24 * 60 * 60
+            || messages.len() == 1
+        {
+            for (idx, message) in messages.iter().enumerate() {
+                if (idx + 1) % 10 == 0 {
+                    println!(
+                        "deleting messages in {guild_id}: {}/{}",
+                        idx + 1,
+                        messages.len()
+                    );
+                }
+
+                self.ctx
+                    .bot
+                    .http
+                    .delete_message(message.channel_id, message.id)
+                    .await?;
+
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
         } else {
             self.ctx
                 .bot
@@ -91,6 +110,8 @@ impl InteractionContext<'_> {
                     .content("done :incoming_envelope:"),
             )
             .await?;
+
+        println!("{guild_id} done");
 
         Ok(())
     }
